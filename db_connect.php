0246d18d43af6f69c69cc696894aa087064db5c9
Rodrigue <94352185+Rodriguemrt@users.noreply.github.com>
2022-11-04 12:31:42 +0100

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "airsport";
-	$conn = mysqli_connect($server, $username, $password, $db);
-	$conn->set_charset("utf8");
-?>
